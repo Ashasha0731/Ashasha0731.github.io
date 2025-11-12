b5253e6dfcdf34381690a1c4c64b305ec46ea241
Ashasha0731 <120244736+Ashasha0731@users.noreply.github.com>
2025-11-13 01:10:46 +0800

    Add files via upload

---
+++ 3-JS/1.1_TV.js
@@ -0,0 +1,12 @@
+export function initSlider(containerId) {
+  const images = [
+    "../0-IMAGE/1導覽頁Guide-page/輪播/1.jpg",
+    "../0-IMAGE/1導覽頁Guide-page/輪播/2.jpg",
+    "../0-IMAGE/1導覽頁Guide-page/輪播/3.jpg",
+    "../0-IMAGE/1導覽頁Guide-page/輪播/4.jpg",
+    "../0-IMAGE/1導覽頁Guide-page/輪播/5.jpg"
+  ];
+
+  const container = document.getElementById(containerId);
+  // ... 初始化輪播程式碼
+}
